70447153f8bc1932b11f359aa147a4d7ecf1f768
Des <d.stout31@yahoo.com>
2020-11-20 21:57:15 -0600

    updated javascript file to fix error in typo and error in array.  updated read me file to include link to repo and image of password gen

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,14 @@
-
 //Array of special characters for random selection into password
-var specialCharacters = ["\\", "<", ">", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "-", "{", "}", "[", "]", "~", ".", "_", "/"];
+var specialCharacters = ['\\', '<', '>', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '?', '-', '{', '}', '[', ']', '~', ',', '.', '_', '/'];
 
 //Array of numbers for random selection into password
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //Array of lowercase alphabet for random selection into password
-var lowerCasedCharachters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Array of uppercase alphabet for random selection into password
-var upperCasedCharacters= ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var upperCasedCharacters = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 //Function to prompt user for password options
 function getPasswordOptions() {
@@ -50,7 +49,7 @@ function getPasswordOptions() {
     hasSpecialCharacters === false &&
     hasNumericCharacters === false &&
     hasLowerCasedCharacters === false &&
-    hasUppercasedCharacters === false
+    hasUpperCasedCharacters === false
     ) {
 
     alert('Must select at least one character type');
@@ -86,34 +85,34 @@ function generatePassword() {
     var possibleCharacters = [];
 
     //Array to contain one of each type of chosen character to ensure each will be used
-    var guaranteedCharachters = [];
+    var guaranteedCharacters = [];
 
     //Conditional statement that adds array of special characters into array of possible charachers based on user input
     //Push new random special character to guaranteedCharacters
     if (options.hasSpecialCharacters) {
       possibleCharacters = possibleCharacters.concat(specialCharacters);
-      guaranteedCharachters.push(getRandom(specialCharacters));
+      guaranteedCharacters.push(getRandom(specialCharacters));
     }
 
     //Conditional statement that adds array of numeric characters into array of possible characters based on user input
     //Push new random special character to guaranteedCharacters
     if (options.hasNumericCharacters) {
       possibleCharacters = possibleCharacters.concat(numericCharacters);
-      guaranteedCharachters.push(getRandom(numericCharacters));
+      guaranteedCharacters.push(getRandom(numericCharacters));
     }
 
     //Conditional statement that adds array of lower cased characters into array of possible characters based on user input
     //Push new random lower-cased character to guaranteedCharacters
     if (options.hasLowerCasedCharacters) {
-      possibleCharacters = possibleCharacters.concat(lowerCasedCharachters);
-      guaranteedCharachters.push(getRandom(lowerCasedCharachters));
+      possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+      guaranteedCharacters.push(getRandom(lowerCasedCharacters));
     }
       
     //Conditional statement that adds array of lower cased characters into array of possible characters based on user input
     //Push new random lower-cased character to guaranteedCharacters
     if (options.hasUpperCasedCharacters) {
       possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
-      guaranteedCharachters.push(getRandom(upperCasedCharacters));
+      guaranteedCharacters.push(getRandom(upperCasedCharacters));
     }
       
     //For loop to iterate over the password length from the options object, selecting random indicies from the array of possible characters and concantenate
@@ -124,8 +123,8 @@ function generatePassword() {
     }
 
     //Mix in at least one of each guaranteed charachter in the result
-    for (var i = 0; i < guaranteedCharachters.length; i++) {
-      result[i] = guaranteedCharachters[i];
+    for (var i = 0; i < guaranteedCharacters.length; i++) {
+      result[i] = guaranteedCharacters[i];
     }
       //Transform the result into a string and pass into writePassword
       return result.join('');
